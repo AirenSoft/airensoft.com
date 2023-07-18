bd5a08c53a8f0756dbfdbd681e14d499715e6b0b
Jeheon Han <hammer99@airensoft.com>
2023-07-18 20:22:10 +0900

    Update product

--- dist/js/bootstrap.bundle.js
+++ dist/js/bootstrap.bundle.js
@@ -7073,3 +7073,12 @@
 
 }));
 //# sourceMappingURL=bootstrap.bundle.js.map
+
+ document.addEventListener("click",function (e){
+   if(e.target.classList.contains("gallery-item")){
+   	  const src = e.target.getAttribute("src");
+   	  document.querySelector(".modal-img").src = src;
+   	  const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+   	  myModal.show();
+   }
+ })
